d9bcb9c739f98554c228134a31df09572917dc80
malekii2003 <123763963+malekii2003@users.noreply.github.com>
2024-04-27 14:22:31 +0330

    Add files via upload

---
+++ Data/HomeData.js
@@ -0,0 +1,100 @@
+const HomeData = [
+  {
+    id: 1,
+    location: "گیلان",
+    model: "ویلا-باغ",
+    price: 1000000,
+    image: "/imgs/1.jpg",
+    discription: "ویلا بسیار شیک و تمیز داری اب برق گاز انترنت و.....",
+    Number: 4,
+  },
+
+  {
+    id: 2,
+    location: "چالوس",
+    model: "کلبه",
+    price: 1200000,
+    image: "/imgs/2.jpg",
+    discription:
+      "کلبه زیبا و دل نشین داری تمامی امکانت و نزدیک به خدمات رفاهی از جمله سوپر مارکت نونوایی",
+    Number: 2,
+  },
+  {
+    id: 3,
+    location: "مازندران",
+    model: "ویلا استخر دار",
+    price: 1500000,
+    image: "/imgs/3.jpg",
+    discription: "ویلا داری استخر اب گرم برای مهمانی و جشن های شما تمیز و شیک",
+    Number: 10,
+  },
+
+  {
+    id: 4,
+    location: "کرج",
+    model: "سوئیت",
+    price: 2000000,
+    image: "/imgs/4.jpg",
+    discription: "سویئت بسیار تمیز دارای یک اتاق خواب طبقه سوم داری آسانسور",
+    Number: 2,
+  },
+  {
+    id: 5,
+    location: "چهارباغ",
+    model: "ویلا",
+    price: 900000,
+    image: "/imgs/5.jpg",
+    discription: "ویلا با فضای باز دارای آلاچیق مناسب مهمانی و جشن تولد",
+    Number: 10,
+  },
+  {
+    id: 6,
+    location: "رشت",
+    model: "کلبه جنگلی",
+    price: 700000,
+    image: "/imgs/6.jpg",
+    discription:
+      "کلبه با ویو جنگل های سر سبز رشت داری بخاری هیزمی مسافت خاکی تا آسفالت 200 متر",
+    Number: 3,
+  },
+  {
+    id: 7,
+    location: "چهابهار",
+    model: "ویلا ساحلی",
+    price: 1200000,
+    image: "/imgs/7.jpg",
+    discription:
+      "کلبه ساحلی با بهترین امکانات رفاهی داری انترنت پر سرعت و نزدیک بودن به ساحل",
+    Number: 4,
+  },
+  {
+    id: 8,
+    location: "شیراز",
+    model: "سویئت",
+    price: 100000,
+    image: "/imgs/8.jpg",
+    discription: "سویئت",
+    Number: 4,
+  },
+  {
+    id: 9,
+    location: "تهران",
+    model: "ویلا",
+    price: 1100000,
+    image: "/imgs/9.jpg",
+    discription:
+      "ویلا بسیار شیک و مرتب داری دو استخر روباز و یک استخر سر پوشیده",
+    Number: 15,
+  },
+  {
+    id: 10,
+    location: "کردان",
+    model: "ویلا",
+    price: 100000,
+    image: "/imgs/10.jpg",
+    discription: "ویلا با ویوی رودخانه تمیز و مرتب",
+    Number: 5,
+  },
+];
+
+export default HomeData;
